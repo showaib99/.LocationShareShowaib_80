7f20da204b3b5523d91ec80e6334df360514363b
Showaib Alam <showaib99@gmail.com>
2024-11-09 19:46:20 +0600

    9November

---
+++ app/src/main/java/com/example/locationshareshowaib_80/model/User.kt
@@ -0,0 +1,24 @@
+package com.example.locationshareshowaib_80.model
+
+
+
+import com.google.firebase.firestore.PropertyName
+
+data class User(
+    val userId: String,
+    @get:PropertyName("displayName")
+    @set:PropertyName("displayName")
+    var displayName: String = "",
+
+    @get:PropertyName("email")
+    @set:PropertyName("email")
+    var email: String = "",
+
+    @get:PropertyName("location")
+    @set:PropertyName("location")
+    var location: String = ""
+) {
+
+    constructor() : this("", "", "")
+}
+
